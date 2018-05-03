f20b7d64e46eca43ab79499c8224331b11f4ca11
delemojw <delemojw@rose-hulman.edu>
2018-05-03 09:36:12 -0400

    Finished person 1 code

--- sandbox_motors_new/person1_motors.py
+++ sandbox_motors_new/person1_motors.py
@@ -70,7 +70,7 @@ def forward_by_time(inches, speed, stop_action):
     """
     left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
     right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
-    time = (((3.14*inches)/360)*speed)
+    time = (inches/speed)*1000
     left_motor.run_timed(speed_sp=speed, time_sp=time)
     right_motor.run_timed(speed_sp=speed, time_sp=time)
 
@@ -101,7 +101,7 @@ def backward_by_time(inches, speed, stop_action):
     """ Calls forward_by_time with negative speeds to achieve backward motion. """
     left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
     right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
-    time = 2 * (360 * inches)
+    time = (inches/speed)*1000
     left_motor.run_timed(speed_sp=-speed, time_sp=time)
     right_motor.run_timed(speed_sp=-speed, time_sp=time)
 def backward_by_encoders(inches, speed, stop_action):
