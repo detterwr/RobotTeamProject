b6eee28d98aae482dcdabcefee45f51b5a77e231
delemojw <delemojw@rose-hulman.edu>
2018-05-14 09:03:15 -0400

    JD

--- sandbox/delemojw/mqtt/m5_pc_remote_drive.py
+++ sandbox/delemojw/mqtt/m5_pc_remote_drive.py
@@ -34,9 +34,9 @@ import robot_controller as robo
 def main():
     # Done: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
     # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy).
-    robot = robo.Snatch3r()
-    mqtt_client = com.MqttClient(robot)
-    mqtt_client.send_message()
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
+
 
     root = tkinter.Tk()
     root.title("MQTT Remote")
@@ -65,8 +65,8 @@ def main():
     forward_button = ttk.Button(main_frame, text="Forward")
     forward_button.grid(row=2, column=1)
     # forward_button and '<Up>' key is done for your here...
-    # forward_button['command'] = lambda: some_callback1(mqtt_client, left_speed_entry, right_speed_entry)
-    # root.bind('<Up>', lambda event: some_callback1(mqtt_client, left_speed_entry, right_speed_entry))
+    forward_button['command'] = lambda: forward(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<Up>', lambda event: forward(mqtt_client, left_speed_entry, right_speed_entry))
 
     left_button = ttk.Button(main_frame, text="Left")
     left_button.grid(row=3, column=0)
@@ -110,7 +110,13 @@ def main():
 # Tkinter callbacks
 # ----------------------------------------------------------------------
 # TODO: 4. Implement the functions for the drive button callbacks.
-
+def forward(mqtt_client, left_speed_entry,right_speed_entry):
+    left_speed_entry.get()
+    print(left_speed_entry.get())
+    right_speed_entry.get()
+    print(right_speed_entry.get())
+    print("I'm reving up!")
+    mqtt_client.send_message("go_forward", (int(left_speed_entry.get()), int(right_speed_entry.get())))
 # TODO: 5. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.  This is the final one!
 #
 # Observations you should make, you did basically this same program using the IR Remote, but your computer can be a
